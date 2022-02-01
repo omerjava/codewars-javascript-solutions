632e41f2479054221130371b081bb61f1953a25a
omer java <87907988+omerjava@users.noreply.github.com>
2022-02-01 03:46:03 +0100

    update

--- reverseMethodsUltimate.js
+++ reverseMethodsUltimate.js
@@ -1,7 +1,7 @@
 
 const reverseMap=a=>a.map((v,i,arr)=>v=arr[arr.length-1-i])
 
-console.log(reverseMap([5,7,2,"re", 6]));
+console.log(reverseMap([5,7,2,"re", 6])); // output : [6, 're', 2, 7, 5]
 
 const reverseReverse=a=>a.reverse();
 
@@ -11,9 +11,9 @@ const reverseMapStr=str=>str.split("").map((v,i,arr)=>v=arr[arr.length-1-i]).joi
 
 const reverseReduceStr=str=>str.split("").reduce((acc,v,i,arr)=>acc+=arr[arr.length-1-i],"")
 
-console.log(reverseReduceStr("stringReverseHello"));
+console.log(reverseReduceStr("stringReverseHello")); // output : olleHesreveRgnirts
 
-console.log(reverseSort([5,7,2,7, 6]));
+console.log(reverseSort([5,7,2,7, 6])); // output : [5, 7, 2, 7, 6]
 
 
 
