29eadfddbe527e872c83f678d997c81e81e5e747
omer java <87907988+omerjava@users.noreply.github.com>
2022-02-01 15:34:27 +0100

    update

--- lonelyIntegerEdabit.js
+++ lonelyIntegerEdabit.js
@@ -6,23 +6,10 @@ lonelyInteger([-3, 1, 2, 3, -1, -4, -2]) ➞ -4
 
 lonelyInteger([-9, -105, -9, -9, -9, -9, 105]) ➞ -9*/
 
-// not completed
+// https://edabit.com/challenge/fK8a63ttMnt9CZmhg
 
 function lonelyInteger(arr){
-    const newArr=[];
-    for(let element of arr){
-        if(element<0){
-            newArr.push(element*-1);
-        }
-        else{
-            newArr.push(element);
-        }
-    }
-    for(let element of newArr){
-        if(newArr.indexOf(element)===newArr.lastIndexOf(element)){
-            return arr.includes(element)?element:element*-1;
-        }
-    }
+    return arr.filter((v,i,array)=>(!array.includes(v*-1)))[0];
 }
 
 console.log(lonelyInteger([-3, 1, 2, 3, -1, -4, -2]));
--- reverseMethodsUltimate.js
+++ reverseMethodsUltimate.js
@@ -1,17 +1,35 @@
 
-const reverseMap=a=>a.map((v,i,arr)=>v=arr[arr.length-1-i])
+const reverseSplitJoin=str=>str.split("").reverse().join("");
 
-console.log(reverseMap([5,7,2,"re", 6])); // output : [6, 're', 2, 7, 5]
+console.log(reverseSplitJoin("reverse1"));
 
-const reverseReverse=a=>a.reverse();
+const reverseForLoop=str=>{
+    let reversed="";
+    for(let i=str.length-1; i>=0; i--){
+        reversed+=str[i];
+    }
+    return reversed;
+}
 
-const reverseSort=a=>a.sort(q=>2); //not working
+console.log(reverseForLoop("reverse2"));
 
 const reverseMapStr=str=>str.split("").map((v,i,arr)=>v=arr[arr.length-1-i]).join("");
 
+console.log(reverseMapStr("reverse3"));
+
 const reverseReduceStr=str=>str.split("").reduce((acc,v,i,arr)=>acc+=arr[arr.length-1-i],"")
 
-console.log(reverseReduceStr("stringReverseHello")); // output : olleHesreveRgnirts
+console.log(reverseReduceStr("reverse4")); 
+
+const reverseMapArray=a=>a.map((v,i,arr)=>v=arr[arr.length-1-i])
+
+console.log(reverseMapArray([5,7,2,"re", 6, "Reverse array with map()"])); // output : [6, 're', 2, 7, 5]
+
+const reverseReverseArray=a=>a.reverse();
+
+console.log(reverseReverseArray([5,7,2,"re", 6, "Reverse array with reverse()"]));
+
+const reverseSort=a=>a.sort(q=>2); //not working
 
 console.log(reverseSort([5,7,2,7, 6])); // output : [5, 7, 2, 7, 6]
 
